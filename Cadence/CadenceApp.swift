8de5580b5af088b4b278b32f93735373eedd24de
Juan Pedro Martín <juamp_m@hotmail.com>
2026-06-12 22:19:42 +0200

    chore(init): Xcode project scaffolding

---
+++ Cadence/CadenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CadenceApp.swift
+//  Cadence
+//
+//  Created by Juan Pedro Martin on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct CadenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CadenceTests/CadenceTests.swift
@@ -0,0 +1,19 @@
+//
+//  CadenceTests.swift
+//  CadenceTests
+//
+//  Created by Juan Pedro Martin on 6/12/26.
+//
+
+import Testing
+@testable import Cadence
+
+struct CadenceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}

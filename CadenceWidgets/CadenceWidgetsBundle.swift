b7a6d6af9c62769fa3ff33f6957c294cc3d09179
Juan Pedro Martín <juamp_m@hotmail.com>
2026-06-26 23:54:41 +0200

    feat(widget): add CadenceWidgets extension target with App Group

---
+++ CadenceWidgets/CadenceWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CadenceWidgetsBundle.swift
+//  CadenceWidgets
+//
+//  Created by Juan Pedro Martin on 6/26/26.
+//
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct CadenceWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        CadenceWidgets()
+    }
+}
